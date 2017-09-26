ee2beee51be9196c5effc924d88e7564de758839
Nicolas <nkao552@gmail.com>
2017-09-26 13:33:18 -0700

    hacking google vision

---
+++ capture.js
@@ -0,0 +1,204 @@
+(function() {
+  // The width and height of the captured photo. We will set the
+  // width to the value defined here, but the height will be
+  // calculated based on the aspect ratio of the input stream.
+
+  var width = 320;    // We will scale the photo width to this
+  var height = 0;     // This will be computed based on the input stream
+
+  // |streaming| indicates whether or not we're currently streaming
+  // video from the camera. Obviously, we start at false.
+
+  var streaming = false;
+
+  // The various HTML elements we need to configure or control. These
+  // will be set by the startup() function.
+
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+
+  function startup() {
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('startbutton');
+
+    navigator.getMedia = ( navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia);
+
+    navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
+
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+      
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
+      
+        if (isNaN(height)) {
+          height = width / (4/3);
+        }
+      
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+      ev.preventDefault();
+    }, false);
+    
+    clearphoto();
+  }
+
+  // Fill the photo with an indication that none has been
+  // captured.
+
+  function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+  
+  // Capture a photo by fetching the current contents of the video
+  // and drawing it into a canvas, then converting that to a PNG
+  // format data URL. By drawing it on an offscreen canvas and then
+  // drawing that to the screen, we can change its size and/or apply
+  // other changes before drawing it.
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    console.log(context);
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      var data = canvas.toDataURL('image/png');
+      sendFileToCloudVision(data);
+      //console.log(data);
+      photo.setAttribute('src', data);
+    } else {
+      clearphoto();
+    }
+  }
+
+  // Set up our event listener to run the startup process
+  // once loading is complete.
+  window.addEventListener('load', startup, false);
+
+// Copyright 2015, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + window.apiKey;
+
+$(function () {
+  $('#fileform').on('submit', uploadFiles);
+});
+
+/**
+ * 'submit' event handler - reads the image bytes and sends it to the Cloud
+ * Vision API.
+ */
+function uploadFiles (event) {
+  event.preventDefault(); // Prevent the default form post
+
+  // Grab the file and asynchronously convert to base64.
+  var file = $('#fileform [name=fileField]')[0].files[0];
+  console.log(file);
+  var reader = new FileReader();
+  reader.onloadend = processFile;
+  reader.readAsDataURL(file);
+}
+
+/**
+ * Event handler for a file's data url - extract the image data and pass it off.
+ */
+function processFile (event) {
+  var content = event.target.result;
+  sendFileToCloudVision(content.replace('data:image/jpeg;base64,', ''));
+}
+
+/**
+ * Sends the given file contents to the Cloud Vision API and outputs the
+ * results.
+ */
+function sendFileToCloudVision (content) {
+  var type = $('#fileform [name=type]').val();
+
+  // Strip out the file prefix when you convert to json.
+  var request = {
+    requests: [{
+      image: {
+        content: content
+      },
+      features: [{
+        type: type,
+        maxResults: 200
+      }]
+    }]
+  };
+
+  $('#results').text('Loading...');
+  $.post({
+    url: CV_URL,
+    data: JSON.stringify(request),
+    contentType: 'application/json'
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    $('#results').text('ERRORS: ' + textStatus + ' ' + errorThrown);
+  }).done(displayJSON);
+}
+
+/**
+ * Displays the results.
+ */
+function displayJSON (data) {
+  var contents = JSON.stringify(data, null, 4);
+  $('#results').text(contents);
+  var evt = new Event('results-displayed');
+  evt.results = contents;
+  document.dispatchEvent(evt);
+}
+
+
+})();
